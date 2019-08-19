52359e40464a8c6cd4eb4c50efaa64362d65a989
beellz <belal20795@gmail.com>
2019-08-19 22:41:16 +0530

    added api with key

--- app.js
+++ app.js
@@ -4,5 +4,21 @@ function addon() {
 
     var search = document.getElementById("Addin");
 
+    let apiKey ="dcbc738b";
+    let urlData = (`http://www.omdbapi.com/?apikey=${apiKey}&t=${search.value}`);
+
     console.log(search.value);
-};
+    console.log(urlData);
+}
+
+// first take search 
+
+
+
+// add the value to api search 
+
+
+// taking the api result 
+
+
+//showing it on display (using the creat node element )
