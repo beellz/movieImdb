3319e98b843b554ecc68791b681db8a283b3880a
beellz <belal20795@gmail.com>
2019-08-20 23:50:08 +0530

    added the api to get down json

--- app.js
+++ app.js
@@ -27,8 +27,19 @@ function getmovies(searchTextall){
     var apiKey ="dcbc738b";
     var urlData = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + searchTextall; 
     console.log(urlData);
+    var OurRequest = new XMLHttpRequest();
+    OurRequest.open('GET', urlData)
+    OurRequest.onload = function() {
+    let ourdata = JSON.parse(OurRequest.responseText);
+    // let meaning =
+        console.log(ourdata);
+    
+    }
+    OurRequest.send();
 }
 // add the value to api search 
+
+
 
 
 // taking the api result 
