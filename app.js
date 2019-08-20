69712d5cf19e869a2eb761727b45a33c88e47d81
beellz <belal20795@gmail.com>
2019-08-20 23:28:34 +0530

    added form tag also to search through

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-console.log("hello");
+// console.log("hello");
 
-function addon() {
+// function addon() {
 
-    var search = document.getElementById("Addin");
+//     var search = document.getElementById("Addin");
 
-    let apiKey ="dcbc738b";
-    let urlData = (`http://www.omdbapi.com/?apikey=${apiKey}&t=${search.value}`);
+//     let apiKey ="dcbc738b";
+//     let urlData = (`http://www.omdbapi.com/?apikey=${apiKey}&t=${search.value}`);
 
-    console.log(search.value);
-    console.log(urlData);
-}
+//     console.log(search.value);
+//     console.log(urlData);
+// }
 
 // first take search 
 
+window.onload = function () {
 
+    document.getElementById("searchform").onsubmit = function(e) {
+       let searchTextall = document.getElementById("searchText").value;
+        getmovies(searchTextall);
+        e.preventDefault();
+    }
+}
 
+function getmovies(searchTextall){
+   
+    var apiKey ="dcbc738b";
+    var urlData = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + searchTextall; 
+    console.log(urlData);
+}
 // add the value to api search 
 
 
