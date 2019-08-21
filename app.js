2e22eb105d20b83e10537c09a6d204fd282dad27
beellz <belal20795@gmail.com>
2019-08-21 20:24:43 +0530

    changed into fetch and also made it show on page

--- app.js
+++ app.js
@@ -14,16 +14,40 @@ function getmovies(searchTextall){
     let apiKey ="dcbc738b";
     let urlData = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + searchTextall; 
     console.log(urlData);
-    let OurRequest = new XMLHttpRequest();
-    OurRequest.open('GET', urlData)
-    OurRequest.onload = function() {
-    let ourdata = JSON.parse(OurRequest.responseText);
-    // let meaning =
-        console.log(ourdata);
-    
-    }
-    OurRequest.send();
+    // let OurRequest = new XMLHttpRequest();
+    // OurRequest.open('GET', urlData)
+    // OurRequest.onload = function() {
+    // let ourdata = JSON.parse(OurRequest.responseText);
+    //  console.log(ourdata);    
+    // }
+    // OurRequest.send();
+         
+// fetch(urlData)
+// .then(function(response){
+//     console.log(response.json);
+// })
+
+fetch(urlData)
+.then(response => response.json())
+.then(data => {
+
+    console.log(data);
+    console.log(data.Poster);
+    let img = document.getElementById('imgShow');
+    let title = document.getElementById('all');
+    let release = document.getElementById('release');
+    title.innerHTML = (data.Title);
+    img.src = (data.Poster); 
+    release.innerHTML = (data.Released);
+//     let output = '<h2>'
+//     let title = Title;
+//     let img = Poster;
+//   console.log(Title);
+//   classAll.innerHTML = title; 
+});
 }
+
+
 // add the value to api search 
 
 
