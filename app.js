fb730e872c6c2d3c5034f05f731b83ac5d4e50bb
beellz <belal20795@gmail.com>
2020-05-13 23:46:50 +0530

    made using the different way need to add link

--- app.js
+++ app.js
@@ -1,42 +1,108 @@
 // first take search 
 window.onload = function() {
-  document.querySelector(".hidden").style.display = 'none';
+  // document.querySelector(".hidden").style.display = 'none';
+  document.getElementById("searchForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  let searchText = document.getElementById("searchText").value
+   getMovies(searchText); 
+  console.log(searchText);
+  })
 };
+
+function getMovies(searchText){
+  console.log("text is " + searchText)
+  let box = document.querySelector(".row").innerHTML
+  axios.get('http://www.omdbapi.com/?apikey=dcbc738b&s=' + searchText)
+  .then((response) => {
+    console.log(response);
+    let moviesAll = response.data.Search;
+    let output = '';
+    // let title = document.querySelector('.row')
+    // title.innerHTML = response.data.Search[0].Title;
+    // console.log(response.data.Search[0].Title);
+
+    
+    // movies.forEach(function(index, movies) {
+      moviesAll.forEach(function(moviesAll){
+      output += `
+      <div class="col-md-3">
+          <div class="well text-center">
+              <img src=${moviesAll.Poster}>
+                <h5>${moviesAll.Title}</h5>
+              
+          </div>
+      </div>
+      `;
+      document.querySelector(".row").innerHTML = output
+    })
+});
+      
+};
+
+
+
+
+
+
+    // let outputall = '';
+
+    //     data.forEach(function(data){
+    //         outputall +=  `
+    //         <ul>
+    //             <li> ID : ${data.id}</li>
+    //             <li> Name : ${data.name}</li>
+    //             <li> username : ${data.username}</li>
+    //             <li> email : ${data.email}
+    //         </ul    
+    //         `;
+    //     })
+
+      
+    //     document.querySelector(".userCall").innerHTML = outputall
+    //     document.querySelector(".userCall").classList.add("box");
+    // };
+
+
+
+
+
 
 // Go button 
 
-let buttonCLick = document.querySelector(".btnSub").addEventListener("click",  function () {
 
-    document.getElementById("searchform").onsubmit = function(e) {
-       let searchTextall = document.getElementById("searchText").value;
-        getmovies(searchTextall);
-        e.preventDefault();
+
+// let buttonCLick = document.querySelector(".btnSub").addEventListener("click",  function () {
+
+//     document.getElementById("searchform").onsubmit = function(e) {
+//        let searchTextall = document.getElementById("searchText").value;
+//         getmovies(searchTextall);
+//         e.preventDefault();
        
-    }
-});
+//     }
+// });
 
 // hidden function program
 
-function hidden() {
-    var x = document.querySelector(".hidden");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-}
+// function hidden() {
+//     var x = document.querySelector(".hidden");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+// }
 
 
 // to get movie from the db using api 
 
-function getmovies(searchTextall){
+// function getmovies(searchTextall){
    
-  let searchKey = searchTextall;
-  let replacedKey = searchKey.split(' ').join('+');
+//   let searchKey = searchTextall;
+//   let replacedKey = searchKey.split(' ').join('+');
 
-    let apiKey ="dcbc738b";
-    let urlData = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=" + replacedKey; 
-    console.log(urlData);
+//     let apiKey ="dcbc738b";
+//     let urlData = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=" + replacedKey; 
+//     console.log(urlData);
     // let OurRequest = new XMLHttpRequest();
     // OurRequest.open('GET', urlData)
     // OurRequest.onload = function() {
@@ -51,29 +117,29 @@ function getmovies(searchTextall){
 // })
 
 
-  fetch(urlData)
-  .then(response => response.json())
-  .then(data => {
+//   fetch(urlData)
+//   .then(response => response.json())
+//   .then(data => {
 
-    console.log(data);
-    console.log(data.Poster);
-    let img = document.getElementById('imgShow');
-    let title = document.getElementById('all');
-    let imdbId = document.getElementById('imdbId');
+//     console.log(data);
+//     console.log(data.Poster);
+//     let img = document.getElementById('imgShow');
+//     let title = document.getElementById('all');
+//     let imdbId = document.getElementById('imdbId');
 
 
-    let release = document.getElementById('release');
-    title.innerHTML = data.Search[0].Title;
-    console.log(title);
-    img.src = data.Search[0].Poster; 
+//     let release = document.getElementById('release');
+//     title.innerHTML = data.Search[0].Title;
+//     console.log(title);
+//     img.src = data.Search[0].Poster; 
      
-    release.innerHTML = data.Search[0].Year;
-    imdbId.innerHTML = data.Search[0].imdbID;
+//     release.innerHTML = data.Search[0].Year;
+//     imdbId.innerHTML = data.Search[0].imdbID;
   
-     hidden();
+//      hidden();
 
-});
-}
+// });
+// }
 
 // use async await
 
